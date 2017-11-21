dbe6941684e64bd7425ab9ccbf3f2eb01f8fc035
Miguel Teixeira <miguelalexbt@users.noreply.github.com>
2017-11-21 17:26:29 +0000

    Added custom cout to exceptions

--- PraiasFluviais/src/Exceptions.h
+++ PraiasFluviais/src/Exceptions.h
@@ -1,4 +1,6 @@
 #include <string>
+#include <iostream>
+
 #ifndef EXCEPTIONS_H_
 #define EXCEPTIONS_H_
 
@@ -15,18 +17,21 @@ public:
 		message = m;
 	}
 
-	/**
-	 * @brief Class constructor
-	 * @return Returns exception's message
-	 */
 	std::string getMessage() const {
 		return message;
 	}
+	virtual void display() const = 0;
+
 };
 
 class FileNotFound : public Exception {
 public:
 	FileNotFound(std::string f) : Exception(f) {
+	}
+
+	void display() const {
+		std::string msg = " Ficheiro não encontrado: " + getMessage();
+		std::cout << msg;
 	}
 };
 
@@ -34,11 +39,21 @@ class PraiaNotFound : public Exception {
 public:
 	PraiaNotFound(std::string n) : Exception(n) {
 	}
+
+	void display() const {
+		std::string msg = " Praia não encontrada: " + getMessage();
+		std::cout << msg;
+	}
 };
 
 class ServicoNotFound : public Exception {
 public:
 	ServicoNotFound(std::string n) : Exception(n) {
+	}
+
+	void display() const {
+		std::string msg = " Serviço não encontrado: " + getMessage();
+		std::cout << msg;
 	}
 };
 
@@ -46,11 +61,21 @@ class ReadingLineError : public Exception {
 public:
 	ReadingLineError(std::string l) : Exception(l) {
 	}
+
+	void display() const {
+		std::string msg = " Erro ao ler a linha: " + getMessage();
+		std::cout << msg;
+	}
 };
 
 class PraiaAlreadyExists : public Exception {
 public:
 	PraiaAlreadyExists(std::string n) : Exception(n) {
+	}
+
+	void display() const {
+		std::string msg = " Praia já existe: " + getMessage();
+		std::cout << msg;
 	}
 };
 
